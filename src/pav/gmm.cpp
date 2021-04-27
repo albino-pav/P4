efbebbca97c1990ab435336990eb260cedcaa5b0
Sergi Izquierdo <564sizquierdo@gmail.com>
2021-04-27 13:30:29 +0200

    .gitignore

--- src/pav/gmm.cpp
+++ src/pav/gmm.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) Universitat Politècnica de Catalunya, Barcelona, Spain.
+/* Copyright (C) Universitat Politï¿½cnica de Catalunya, Barcelona, Spain.
  *
  * Permission to copy, use, modify, sell and distribute this software
  * is granted provided this copyright notice appears in all copies.
@@ -209,6 +209,12 @@ namespace upc {
 	  //
       // Update old_prob, new_prob and inc_prob in order to stop the loop if logprob does not
       // increase more than inc_threshold.
+      new_prob = em_expectation(data, weights);
+      em_maximization(data, weights);
+
+      inc_prob=new_prob - old_prob;
+      old_prob=new_prob;
+
       if (verbose & 01)
 	cout << "GMM nmix=" << nmix << "\tite=" << iteration << "\tlog(prob)=" << new_prob << "\tinc=" << inc_prob << endl;
     }
