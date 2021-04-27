f19dcd4b4619de408c75962544488836370665c7
Antoni-Joan Solergibert i Llaquet <antoni-joan.solergibert@estudiantat.upc.edu>
2021-04-27 12:37:02 +0200

    Ex Arreglados

--- scripts/wav2lpcc.sh
+++ scripts/wav2lpcc.sh
@@ -46,7 +46,7 @@ sox $inputfile -t raw -e signed -b 16 - | $X2X +sf | $FRAME -l 240 -p 80 | $WIND
 	$LPC -l 240 -m $lpc_order | $LPC2C -m $lpc_order -M $cepstrum_order > $base.lpcc
 
 # Our array files need a header with the number of cols and rows:
-ncol=$((cepstrum_order)) # lpc p =>  (gain a1 a2 ... ap) !!!Preguntar Albino!!!
+ncol=$((cepstrum_order + 1)) # lpc p =>  (gain a1 a2 ... ap) !!!Preguntar Albino!!!
 nrow=`$X2X +fa < $base.lpcc | wc -l | perl -ne 'print $_/'$ncol', "\n";'`
 
 # Build fmatrix file by placing nrow and ncol in front, and the data after them
--- scripts/wav2mfcc.sh
+++ scripts/wav2mfcc.sh
@@ -40,7 +40,7 @@ fi
 
 # Main command for feature extration
 sox $inputfile -t raw -e signed -b 16 - | $X2X +sf | $FRAME -l 240 -p 80 | $WINDOW -l 240 -L 240 |
-	$MFCC -l 240 -m $mfcc_order > $base.mfcc
+	$MFCC -l 240 -m $mfcc_order -w 1 > $base.mfcc
 
 # Our array files need a header with the number of cols and rows:
 ncol=$((mfcc_order)) # lpc p =>  (gain a1 a2 ... ap) EN ESTE CASO MFCC SIN EL 1??? Preguntar Albino!
