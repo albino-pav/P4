c2f1542d1d57e819059e188bea3214719dc68def
Sergi Izquierdo <564sizquierdo@gmail.com>
2021-05-02 20:21:43 +0200

    run_spkid

--- scripts/wav2mfcc.sh
+++ scripts/wav2mfcc.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-## \file
-## \TODO This file implements a very trivial feature extraction; use it as a template for other front ends.
-## 
-## Please, read SPTK documentation and some papers in order to implement more advanced front ends.
-
 # Base name for temporary files
-base=/tmp/$(basename $0).$$ 
+base=/tmp/$(basename $0).$$
 
 # Ensure cleanup of temporary files on exit
 trap cleanup EXIT
@@ -14,8 +9,8 @@ cleanup() {
    \rm -f $base.*
 }
 
-if [[ $# != 3 ]]; then
-   echo "$0 mfcc_order input.wav output.mfcc"
+if [[ $# != 4 ]]; then
+   echo "$0 mfcc_order bank_size input.wav output.lp"
    exit 1
 fi
 
@@ -24,27 +19,30 @@ bank_size=$2
 inputfile=$3
 outputfile=$4
 
+UBUNTU_SPTK=1
 if [[ $UBUNTU_SPTK == 1 ]]; then
    # In case you install SPTK using debian package (apt-get)
    X2X="sptk x2x"
    FRAME="sptk frame"
    WINDOW="sptk window"
+   LPC="sptk lpc"
    MFCC="sptk mfcc"
 else
    # or install SPTK building it from its source
    X2X="x2x"
    FRAME="frame"
    WINDOW="window"
+   LPC="lpc"
    MFCC="mfcc"
 fi
 
 # Main command for feature extration
 sox $inputfile -t raw -e signed -b 16 - | $X2X +sf | $FRAME -l 180 -p 100 | $WINDOW -l 180 -L 180 |
-	$MFCCC -l 180 -m $mfcc_order -n $bank_size  $base.mfcc
+	$MFCC -l 180 -m $mfcc_order -n $bank_size > $base.mfcc
 
 # Our array files need a header with the number of cols and rows:
-ncol=$((mfcc_order)) # mfcc p =>  (gain a1 a2 ... ap) 
-nrow=`$X2X +fa < $base.mfcc | wc -l | perl -ne 'print $_/'$ncol', "\n";'`
+ncol=$((mfcc_order)) # mfcc
+nrow=$($X2X +fa < $base.mfcc | wc -l | perl -ne 'print $_/'$ncol', "\n";')
 
 # Build fmatrix file by placing nrow and ncol in front, and the data after them
 echo $nrow $ncol | $X2X +aI > $outputfile
