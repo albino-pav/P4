306761bf6b0f070036be39ddbb94fa7720077883
Sergi Izquierdo <564sizquierdo@gmail.com>
2021-05-03 18:32:12 +0200

    deures2

--- src/pav/gmm.cpp
+++ src/pav/gmm.cpp
@@ -101,6 +101,7 @@ namespace upc {
   }
 
   /// \TODO Compute the logprob for the whole input data.
+  /// \DONE
   float GMM::logprob(const fmatrix &data) const {    
 
     if (nmix == 0 or vector_size == 0 or vector_size != data.ncol())
@@ -111,6 +112,7 @@ namespace upc {
 
     for (n=0; n<data.nrow(); ++n) {
       /// \TODO Compute the logprob of a single frame of the input data; you can use gmm_logprob() above.
+      /// \DONE
       lprob += gmm_logprob(data[n]);
     }    
     return lprob/n;
