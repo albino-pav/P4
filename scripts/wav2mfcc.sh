1fd4b3e31263b14bd1215673533d6a2a7fd7d46c
Laura Rayón <59146632+laurayon@users.noreply.github.com>
2021-05-06 10:36:02 +0200

    Update wav2mfcc.sh

--- scripts/wav2mfcc.sh
+++ scripts/wav2mfcc.sh
@@ -2,8 +2,7 @@
 
 ## \file
 ## \TODO This file implements a very trivial feature extraction; use it as a template for other front ends.
-## 
-## Please, read SPTK documentation and some papers in order to implement more advanced front ends.
+## /DONE Implemented mfcc calculus 
 
 # Base name for temporary files
    #basename te da el nombre del fichero con su extensión
@@ -22,7 +21,7 @@ if [[ $# != 3 ]]; then
    exit 1
 fi
 
-mfcc_order=$1 #GOOD VALUE = 13
+mfcc_order=$1 #GOOD VALUE = 25
 inputfile=$2
 outputfile=$3
 
@@ -49,24 +48,24 @@ sox $inputfile -t raw -e signed -b 16 - | #convert (-t) wav file to raw file
                                           #con desplazamiento de 80 muestras
     $WINDOW -l 240 -L 240 |               #enventana con Blackman longitud 240
                                           #-l frame length input, -L frame length output
-	$MFCC -m $mfcc_order -l 240 > $base.mfc  #computes LPC cepstral coeffs from LPC
+	$MFCC -m $mfcc_order -l 240 > $base.mfcc  #computes LPC cepstral coeffs from LPC
 
 # Our array files need a header with the number of cols and rows:
 #Esto se hace para que haga match con el formato fmatrix
 ncol=$((mfcc_order)) #gives cepstrum_order coefficients as output 
 #El numero de filas = numero de tramas
-   #convierte el asci leido de base.mfc a float 
+   #convierte el asci leido de base.mfcc a float 
    #el resultado lo pasa a wc -l, que cuenta las lineas que hay en el fichero
    #perl -ne es un while que recorre lineas y te printea en el fichero base.mfcc
       #el número correspondiente para cada    
 
-nrow=`$X2X +fa < $base.mfc | wc -l | perl -ne 'print $_/'$ncol', "\n";'`
+nrow=`$X2X +fa < $base.mfcc | wc -l | perl -ne 'print $_/'$ncol', "\n";'`
 
 # Build fmatrix file by placing nrow and ncol in front, and the data after them
 #x2x +aI conversión de auscii a unsigned int (4bytes) de los valores de fila y columna
 echo $nrow $ncol | $X2X +aI > $outputfile
 # cat - Copy standard input to standard output.-> copia el fichero base.mfc 
    #al final de outputfile (<<) sin cambiar nada. 
-cat $base.mfc >> $outputfile
+cat $base.mfcc >> $outputfile
 
 exit
