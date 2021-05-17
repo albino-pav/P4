7639237d0fb7f5f971e76c1939a2f5a73329fb53
Adrià Solana Carulla <adria.solana@estudiantat.upc.edu>
2021-05-17 09:56:12 +0200

    Codi per tasca 3.5

--- scripts/graphs.py
+++ scripts/graphs.py
@@ -1,32 +1,48 @@
 import matplotlib.pyplot as plt
 import soundfile as sf
 import numpy as np
+import os
+
+os.getcwd()
+path = os.getcwd()
+
+lpFile = open(path + '/lp_2_3.txt', 'r')
+lpLines = lpFile.read().splitlines()
+c2lp = []
+c3lp = []
+for x in lpLines:
+    c2lp.append(x.split('\t')[0])
+    c3lp.append(x.split('\t')[1])
+lpFile.close()
+
+lpccFile = open(path + '/lpcc_2_3.txt', 'r')
+lpccLines = lpccFile.read().splitlines()
+c2lpcc = []
+c3lpcc = []
+for x in lpccLines:
+    c2lpcc.append(x.split('\t')[0])
+    c3lpcc.append(x.split('\t')[1])
+lpccFile.close()
+
+mfccFile = open(path + '/mfcc_2_3.txt', 'r')
+mfccLines = mfccFile.read().splitlines()
+c2mfcc = []
+c3mfcc = []
+for x in mfccLines:
+    c2mfcc.append(x.split('\t')[0])
+    c3mfcc.append(x.split('\t')[1])
+mfccFile.close()
 
 plt.figure(1)
-lpFile = open('lp_2_3.txt', 'r')
-lp = np.loadtxt(lpFile, usecols = (0,2))
-plt.plot(lp(0), lp(1))
-plt.show()
-
-plt.figure(2)
-lpccFile = open('lpcc_2_3.txt', 'r')
-lpcc = np.loadtxt(lpccFile, usecols = (0,2))
-plt.plot(lpcc(0), lpcc(1))
-plt.show()
-
-plt.figure(3)
-mfccFile = open('mfcc_2_3.txt', 'r')
-mfcc = np.loadtxt(mfccFile, usecols = (0,2))
-plt.plot(mfcc(0), mfcc(1))
-plt.show()
-
-plt.figure(3)
 fig, axs = plt.subplots(3)
 fig.suptitle('LP, LPCC and MFCC 2nd and 3rd coefficients')
-axs[0].plot(lp(0), lp(1))
+axs[0].scatter(c2lp, c3lp, s = 1)
 axs[0].set_title('LP')
-axs[1].plot(lpcc(0), lpcc(1))
+axs[0].tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, labelleft=False)  
+axs[1].scatter(c2lpcc, c3lpcc, s = 1)
 axs[1].set_title('LPCC')
-axs[2].plot(mfcc(0), mfcc(1))
+axs[1].tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, labelleft=False) 
+axs[2].scatter(c2mfcc, c3mfcc, s = 1)
 axs[2].set_title('MFCC')
+axs[2].tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, labelleft=False) 
 plt.show()
